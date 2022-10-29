843b4df6589f43ea971a6799232ed474a0743325
Roberto Rebello <roberto.rebello@gmail.com>
2022-10-29 00:04:07 -0300

    Adding minimum Slider functions

--- QRangeSlider.cpp
+++ QRangeSlider.cpp
@@ -3,12 +3,7 @@
 #include <QPainter>
 #include <QMouseEvent>
 
-QRangeSlider::QRangeSlider(QWidget *parent) : QWidget(parent),
-                                              m_sliderSize(5),
-                                              m_handleSize(13),
-                                              m_padding(0),
-                                              m_minimum(0),
-                                              m_maximum(100)
+QRangeSlider::QRangeSlider(QWidget *parent) : QWidget(parent)
 {
     m_lowValue = m_minimum;
     m_highValue = m_maximum;
@@ -18,6 +13,134 @@ QRangeSlider::QRangeSlider(QWidget *parent) : QWidget(parent),
 
 QRangeSlider::~QRangeSlider()
 {
+}
+
+unsigned int QRangeSlider::minimum() const
+{
+    return m_minimum;
+}
+
+void QRangeSlider::setMinimum(const unsigned int minimum)
+{
+    if (m_minimum != minimum)
+    {
+        m_minimum = minimum;
+        if (m_minimum >= m_maximum)
+        {
+            setMaximum(m_minimum + 1);
+            setHighValue(m_maximum);
+            setLowValue(m_minimum);
+        }
+        else if (m_minimum >= m_highValue)
+        {
+            setHighValue(m_minimum + 1);
+            setLowValue(m_minimum);
+        }
+        else if (m_minimum > m_lowValue)
+        {
+            setLowValue(m_minimum);
+        }
+
+        update();
+        emit(rangeChange(m_minimum, m_maximum));
+    }
+}
+
+unsigned int QRangeSlider::maximum() const
+{
+    return m_maximum;
+}
+
+void QRangeSlider::setMaximum(const unsigned int maximum)
+{
+    if (maximum != m_maximum)
+    {
+        m_maximum = maximum;
+        if (m_maximum <= m_minimum)
+        {
+            setMinimum(m_maximum++);
+            setHighValue(m_maximum);
+            setLowValue(m_minimum);
+        }
+        else if (m_maximum <= m_lowValue)
+        {
+            setHighValue(m_maximum);
+            setLowValue(m_highValue - 1);
+        }
+        else if (m_maximum < m_highValue)
+        {
+            setHighValue(m_maximum);
+        }
+
+        update();
+        emit(rangeChange(m_minimum, m_maximum));
+    }
+}
+
+unsigned int QRangeSlider::lowValue() const
+{
+    return m_lowValue;
+}
+
+void QRangeSlider::setLowValue(const unsigned int lowValue)
+{
+    if (m_lowValue != lowValue)
+    {
+        m_lowValue = lowValue;
+        if (m_lowValue > m_maximum)
+        {
+            m_lowValue = m_maximum - 1;
+        }
+        if (m_lowValue < m_minimum)
+        {
+            m_lowValue = m_minimum;
+        }
+        if (m_lowValue > m_highValue)
+        {
+            setHighValue(m_lowValue + 1);
+        }
+
+        update();
+        emit(lowValueChange(m_lowValue));
+    }
+}
+
+unsigned int QRangeSlider::highValue() const
+{
+    return m_highValue;
+}
+
+void QRangeSlider::setHighValue(const unsigned int highValue)
+{
+    if (m_highValue != highValue)
+    {
+        m_highValue = highValue;
+        if (m_highValue > m_maximum)
+        {
+            m_highValue = m_maximum;
+        }
+        if (m_highValue < m_minimum)
+        {
+            m_highValue = m_minimum + 1;
+        }
+        if (m_highValue < m_lowValue)
+        {
+            setLowValue(m_lowValue - 1);
+        }
+
+        update();
+        emit(highValueChange(m_highValue));
+    }
+}
+
+unsigned int QRangeSlider::step() const
+{
+    return m_step;
+}
+
+void QRangeSlider::setStep(const unsigned int step)
+{
+    m_step = step;
 }
 
 void QRangeSlider::paintEvent(QPaintEvent *event)
@@ -50,7 +173,7 @@ void QRangeSlider::paintEvent(QPaintEvent *event)
     // Draw lower handle
     painter.setPen(QPen(Qt::GlobalColor::darkGray, 0.8));
     painter.setBrush(QBrush(QColor(Qt::GlobalColor::white)));
-    painter.drawRoundedRect(m_padding + ((width() - 2 * m_padding) * (m_lowValue - m_minimum) / (m_maximum - m_minimum)) - (m_handleSize / 2),
+    painter.drawRoundedRect(m_padding + ((width() - 2 * m_padding) * (m_lowValue - m_minimum) / (m_maximum - m_minimum)),
                             (height() - m_handleSize) / 2,
                             m_handleSize,
                             m_handleSize,
@@ -58,7 +181,7 @@ void QRangeSlider::paintEvent(QPaintEvent *event)
                             2);
 
     // Draw higher handle
-    painter.drawRoundedRect(m_padding + ((width() - 2 * m_padding) * (m_highValue - m_minimum) / (m_maximum - m_minimum)) - (m_handleSize / 2),
+    painter.drawRoundedRect(m_padding + ((width() - 2 * m_padding) * (m_highValue - m_minimum) / (m_maximum - m_minimum)) - m_handleSize,
                             (height() - m_handleSize) / 2,
                             m_handleSize,
                             m_handleSize,
--- QRangeSlider.hpp
+++ QRangeSlider.hpp
@@ -10,28 +10,46 @@ public:
     ~QRangeSlider();
 
 private:
+    unsigned int minimum() const;
+    void setMinimum(const unsigned int minimum);
+    unsigned int maximum() const;
+    void setMaximum(const unsigned int maximum);
+    unsigned int lowValue() const;
+    void setLowValue(const unsigned int lowValue);
+    unsigned int highValue() const;
+    void setHighValue(const unsigned int highValue);
+    unsigned int step() const;
+    void setStep(const unsigned int step);
     void paintEvent(QPaintEvent *event);
     QSize sizeHint() const;
     QSize minimumSizeHint() const;
 
     /* Slider size in pixels */
-    unsigned int m_sliderSize;
+    unsigned int m_sliderSize = 5;
 
     /* Slider handle size in pixels */
-    unsigned int m_handleSize;
+    unsigned int m_handleSize = 13;
 
     /* Left and Right padding in pixels */
-    unsigned int m_padding;
+    unsigned int m_padding = 1;
 
     /* Minmum range value */
-    unsigned int m_minimum;
+    unsigned int m_minimum = 0;
 
     /* Maximum range value */
-    unsigned int m_maximum;
+    unsigned int m_maximum = 100;
+
+    /* Step value */
+    unsigned int m_step = 1;
 
     /* Value of low handle */
     unsigned int m_lowValue;
 
     /* Value of high handle */
     unsigned int m_highValue;
+
+signals:
+    void rangeChange(unsigned int minimum, unsigned int maximum);
+    void lowValueChange(unsigned int lowValue);
+    void highValueChange(unsigned int highValue);
 };
